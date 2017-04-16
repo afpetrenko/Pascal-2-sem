d383c0ce1404ad496239506e4b6074cb2acf8a1c
Alexander <afpetrenko@yandex.ru>
2017-04-16 17:04:40 +0300

    lab23

--- lab23/lab23.pas
+++ lab23/lab23.pas
@@ -1,10 +1,15 @@
 Program lab23;
 Uses unitmax;
+var
+  i : byte;
 
 Begin
-  input();
-  max();
-  print();
+  Writeln('¬ведите кол-во студентов');
+  Readln(i);
+  input(i);
+  readfile(mas);
+  print(i);
+  max(i);
   write();
 end.
 
--- lab23/unitmax.pas
+++ lab23/unitmax.pas
@@ -1,83 +1,118 @@
 Unit unitmax;
 Interface
+
 Type
   rec = record
-  name2, name : string [20];
-  sch, bal : byte;
+  name2, name : string[20];
+  sch, bal : integer;
   end;
-  tmas = array[1..3] of rec;
+  tmas = array[1..4] of rec;
 var
   mas, masx : tmas;
-  k : byte;
+  m : rec;
+  k, i : byte;
+  f : file of rec;
 
-Procedure input();
-Procedure max();
+Procedure input(var k: byte);
+Procedure max(var j: byte);
 Procedure write();
+Procedure readfile(var a: tmas);
+Procedure print(var k: byte);
 
   Implementation
-Procedure max();
+  
+  
+Procedure max(var j: byte);
 var
-i, j: byte;
+i: byte;
 Begin
 i := 1;
 k := 1;
 with mas[1], rec do
-  writeln(mas[1].bal);
-masx[k] := mas[1];
-for i := 1 to 2 do
+  writeln('бал первого', mas[1].bal);
+masx[1] := mas[1];
+for i := 1 to (j-1) do
   begin
     if mas[i].bal <= mas[i+1].bal 
       then
         begin
-         masx[k] := mas[i+1]; 
-         inc(k);
+         masx[1] := mas[i+1];
+         writeln('номер масх', k);
+         end;
+  end;
+  
+for i := 1 to j do 
+  begin
+  if mas[i].bal = masx[1].bal then
+    begin
+      inc(k);
+      masx[k] := mas[i];
+    end;
   end;
 with masx, rec do
-  writeln('Модуль', masx[k].bal);
+  writeln('Макс', masx[k].bal);
 end;
-end;
 
 
 
 
-  Procedure input();
+
+
+Procedure readfile(var a: tmas);
+var
+  k : byte;
 Begin
-    with mas[1], rec do
-        begin
-          name := 'Ivan';
-          name2 := 'Ivanov';
-          sch := 54;
-          bal := 59;
-        end;
-    writeln(mas[1].name, ' ', mas[1].name2, ' ', mas[1].sch, ' ', mas[1].bal);
-    with mas[2], rec do
+  k := 0;
+  Reset(f);
+  while not eof(f) do
+  begin
+    inc(k);
+    read(f, a[k]);   
+  end;
+  Close(f);
+end;  
+
+
+
+
+
+  Procedure input(var k: byte);
+var
+  i : byte;
+Begin
+    Assign(f, 'file.txt');
+    Rewrite(f);
+    for i := 1 to k do
       begin
-        name := 'Petr';
-          name2 := 'Petrov';
-          sch := 64;
-          bal := 96;
-        end;
-    writeln(mas[2].name, ' ', mas[2].name2, ' ', mas[2].sch, ' ', mas[2].bal);
-    with mas[3], rec do
+        writeln(i, ' Студент');
+        writeln('Имя:'); Readln(m.name);
+        writeln('Фамилия:'); Readln(m.name2);
+        writeln('Школа:'); Readln(m.sch);
+        writeln('Кол-во баллов:'); Readln(m.bal);
+        write(f, m); 
+      end;
+    close(f);
+end;      
+      
+
+
+
+
+  Procedure print(var k: byte);
+var
+  l : byte;
+begin
+    for l := 1 to k do 
       begin
-        name := 'Dima';
-          name2 := 'Dimenko';
-          sch := 32;
-          bal := 96;
-          end;
-          writeln(mas[3].name, ' ', mas[3].name2, ' ', mas[3].sch, ' ', mas[3].bal);
+        writeln(mas[l].name, ' ', mas[l].name2, ' ', mas[l].sch, ' ', mas[l].bal);
+      end;
+    for l := 1 to 4 do
+      writeln;   
+    writeln; 
 end;
 
-  Procedure print();
-  var
-    i : byte;
-    
-begin
-    for i := 1 to 3 do 
-      begin
-        writeln(mas[i].name, ' ', mas[i].name2, ' ', mas[i].sch, ' ', mas[i].bal);
-        end;
-end;
+
+
 
 
   Procedure write();
@@ -88,7 +123,7 @@ Begin
   assign(f, 'f.txt');
   rewrite(f);
   writeln(f, 'Максимальное количество баллов:');
-  for i := 1 to k do
+  for i := 2 to k do
   writeln(f, masx[i].name, ' ', masx[i].name2, ' ', masx[i].sch, ' ', masx[i].bal);
   close(f);
 end;
